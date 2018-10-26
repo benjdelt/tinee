197f53c76871c95662598a07a4eb9bef6f477683
Ben Deltenre <benjdelt@gmail.com>
2018-10-26 09:53:17 -0700

    Added dotenv package and required it in the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = require('./routes/routes.js');
 const path = require('path');
 
+require('dotenv').config();
+
 const app = express();
 
 app.set('view engine', 'ejs');
