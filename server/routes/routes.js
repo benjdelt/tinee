9efc9412fcbcb7969e35dfc59328a0ea7607977d
Ben Deltenre <benjdelt@gmail.com>
2018-10-26 17:03:44 -0700

    Added models to router

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+const User = require('../../models/User');
+const Url = require('../../models/Url');
+
 
 router.get('/', (req, res) => {
   res.render('index');
