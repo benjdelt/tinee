3aad7bce078cae9905ab10acb5f68fa8e6aca874
Ben Deltenre <benjdelt@gmail.com>
2018-10-31 16:55:42 -0700

    Styled url buttons

--- client/components/urls.js
+++ client/components/urls.js
@@ -3,39 +3,50 @@ import MicrolinkCard from 'react-microlink';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
+import Copy from '@material-ui/icons/FileCopy';
+import Edit from '@material-ui/icons/Create';
+import Delete from '@material-ui/icons/Delete';
+
 class Urls extends Component {
   render() {
     return (
       <div>
+        <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
         <MicrolinkCard
           url='https://www.theverge.com/tldr/2018/2/7/16984284/tesla-space-falcon-heavy-launch-elon-musk'
-          // size='large'
           target='_blank'
         />
-        <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
+        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button> 
+        <Button color="primary"><Copy/>Copy</Button>
+        <Button color="primary"><Edit />Edit</Button>
+        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
         <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
         <MicrolinkCard 
           url='https://medium.freecodecamp.org/learning-python-from-zero-to-hero-120ea540b567?fbclid=IwAR1GY8Y7RcG0uK_wuPuFhmqV6xV82wupRqh5GQ5NPKd8GDsTkD3KrDuex9E'
           target='_blank'
         />
-        <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
+        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
+        <Button color="primary"><Copy/>Copy</Button>
+        <Button color="primary"><Edit />Edit</Button>
+        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
         <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
         <MicrolinkCard
           url='https://docs.mongodb.com/manual/mongo/'
           target='_blank'
         />
-        <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
+        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
+        <Button color="primary"><Copy/>Copy</Button>
+        <Button color="primary"><Edit />Edit</Button>
+        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
         <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
         <MicrolinkCard
           url='https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3'
           target='_blank'
         />
-        <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
-        <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
+        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
+        <Button color="primary"><Copy/>Copy</Button>
+        <Button color="primary"><Edit />Edit</Button>
+        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
       </div>
     )
   }
