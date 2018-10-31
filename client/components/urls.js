9335fe78711f7d665c8b7ec8469feccc0ef44073
Ben Deltenre <benjdelt@gmail.com>
2018-10-31 16:07:09 -0700

    Styled urls components

--- client/components/urls.js
+++ client/components/urls.js
@@ -13,28 +13,28 @@ class Urls extends Component {
           target='_blank'
         />
         <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button>Copy</Button><Button>Edit</Button><Button>Delete</Button>
+        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
         <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
         <MicrolinkCard 
           url='https://medium.freecodecamp.org/learning-python-from-zero-to-hero-120ea540b567?fbclid=IwAR1GY8Y7RcG0uK_wuPuFhmqV6xV82wupRqh5GQ5NPKd8GDsTkD3KrDuex9E'
           target='_blank'
         />
         <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button>Copy</Button><Button>Edit</Button><Button>Delete</Button>
+        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
         <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
         <MicrolinkCard
           url='https://docs.mongodb.com/manual/mongo/'
           target='_blank'
         />
         <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button>Copy</Button><Button>Edit</Button><Button>Delete</Button>
+        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
         <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
         <MicrolinkCard
           url='https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3'
           target='_blank'
         />
         <Typography variant="body1">
-        <a href="#">/abd123</a></Typography> <Button>Copy</Button><Button>Edit</Button><Button>Delete</Button>
+        <a href="#">/abd123</a></Typography> <Button color="inherit">Copy</Button><Button color="inherit">Edit</Button><Button color="inherit">Delete</Button>
         <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
       </div>
     )
