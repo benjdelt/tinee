0f54c3a84da6095987e024749c11677f9a61e0cb
Ben Deltenre <benjdelt@gmail.com>
2018-10-31 17:06:15 -0700

    Put url components in grid

--- client/components/urls.js
+++ client/components/urls.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import MicrolinkCard from 'react-microlink';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
+import Grid from '@material-ui/core/Grid';
 import Copy from '@material-ui/icons/FileCopy';
 import Edit from '@material-ui/icons/Create';
 import Delete from '@material-ui/icons/Delete';
@@ -10,44 +10,60 @@ import Delete from '@material-ui/icons/Delete';
 class Urls extends Component {
   render() {
     return (
-      <div>
-        <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
-        <MicrolinkCard
-          url='https://www.theverge.com/tldr/2018/2/7/16984284/tesla-space-falcon-heavy-launch-elon-musk'
-          target='_blank'
-        />
-        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button> 
-        <Button color="primary"><Copy/>Copy</Button>
-        <Button color="primary"><Edit />Edit</Button>
-        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
-        <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
-        <MicrolinkCard 
-          url='https://medium.freecodecamp.org/learning-python-from-zero-to-hero-120ea540b567?fbclid=IwAR1GY8Y7RcG0uK_wuPuFhmqV6xV82wupRqh5GQ5NPKd8GDsTkD3KrDuex9E'
-          target='_blank'
-        />
-        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
-        <Button color="primary"><Copy/>Copy</Button>
-        <Button color="primary"><Edit />Edit</Button>
-        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
-        <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
-        <MicrolinkCard
-          url='https://docs.mongodb.com/manual/mongo/'
-          target='_blank'
-        />
-        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
-        <Button color="primary"><Copy/>Copy</Button>
-        <Button color="primary"><Edit />Edit</Button>
-        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
-        <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
-        <MicrolinkCard
-          url='https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3'
-          target='_blank'
-        />
-        <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
-        <Button color="primary"><Copy/>Copy</Button>
-        <Button color="primary"><Edit />Edit</Button>
-        <Button variant="outlined" color="secondary"><Delete />Delete</Button>
-      </div>
+      <Grid container spacing={24}>
+        <Grid item xs>
+  
+          <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
+          <MicrolinkCard
+            url='https://www.theverge.com/tldr/2018/2/7/16984284/tesla-space-falcon-heavy-launch-elon-musk'
+            target='_blank'
+          />
+          <Button color="primary" variant="outlined" href="/abd123">/abd123</Button> 
+          <Button color="primary"><Copy/>Copy</Button>
+          <Button color="primary"><Edit />Edit</Button>
+          <Button variant="outlined" color="secondary"><Delete />Delete</Button>
+        </Grid>
+           
+        <Grid item xs>
+
+          <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
+          <MicrolinkCard 
+            url='https://medium.freecodecamp.org/learning-python-from-zero-to-hero-120ea540b567?fbclid=IwAR1GY8Y7RcG0uK_wuPuFhmqV6xV82wupRqh5GQ5NPKd8GDsTkD3KrDuex9E'
+            target='_blank'
+          />
+          <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
+          <Button color="primary"><Copy/>Copy</Button>
+          <Button color="primary"><Edit />Edit</Button>
+          <Button variant="outlined" color="secondary"><Delete />Delete</Button>
+        </Grid>
+
+        <Grid item xs>
+  
+          <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
+          <MicrolinkCard
+            url='https://docs.mongodb.com/manual/mongo/'
+            target='_blank'
+          />
+          <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
+          <Button color="primary"><Copy/>Copy</Button>
+          <Button color="primary"><Edit />Edit</Button>
+          <Button variant="outlined" color="secondary"><Delete />Delete</Button>
+        </Grid>
+
+        <Grid item xs>
+
+          <Typography variant='caption'>Created OCT 29, 3:05 PM</Typography>
+          <MicrolinkCard
+            url='https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3'
+            target='_blank'
+          />
+          <Button color="primary" variant="outlined" href="/abd123">/abd123</Button>
+          <Button color="primary"><Copy/>Copy</Button>
+          <Button color="primary"><Edit />Edit</Button>
+          <Button variant="outlined" color="secondary"><Delete />Delete</Button>
+        </Grid>
+        
+      </Grid>
     )
   }
 }
