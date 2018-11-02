d275bb65a2c88165479b0013e8e68e9f7b39d347
Ben Deltenre <benjdelt@gmail.com>
2018-11-02 16:16:27 -0700

    Made Edit dialog component

--- client/components/App.js
+++ client/components/App.js
@@ -4,6 +4,7 @@ import '../css/App.css';
 import Paper from '@material-ui/core/Paper';
 import Navbar from './nav';
 import Urls from './urls'
+import EditUrl from './edit-url';
 import ShortenForm from './shorten-form'
 import Footer from './footer';
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
         <Navbar></Navbar>
         <Paper elevation={3} style={{width: '85%', padding: 25, margin: "20px auto 0"}}>
           <ShortenForm></ShortenForm>
+          <EditUrl></EditUrl>
           <Urls></Urls>
           <Footer></Footer>
         </Paper>
---
+++ client/components/edit-url.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Edit from '@material-ui/icons/Create';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import TextField from '@material-ui/core/TextField';
+
+function Transition(props) {
+  return <Slide direction="down" {...props} />;
+}
+
+class EditUrl extends React.Component {
+  
+  constructor (props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    // this.handleClickOpen = this.handleClickOpen.bind(this);
+    // this.handleClickClose = this.handleClickClose.bind(this);
+  }
+  
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.handleClickOpen} color="primary"><Edit />Edit</Button>
+        <Dialog
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleClose}
+        >
+          <DialogTitle>
+            {"Edit URL"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Long URL"
+                fullWidth
+              />
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="inherit">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Update
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default EditUrl;
