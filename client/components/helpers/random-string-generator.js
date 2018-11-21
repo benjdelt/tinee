328482160f830fd4eee91c1401c6095f45b85056
Ben Deltenre <benjdelt@gmail.com>
2018-11-21 10:24:57 -0800

    Updated random string generator

--- client/components/helpers/random-string-generator.js
+++ client/components/helpers/random-string-generator.js
@@ -2,13 +2,14 @@
 // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
 
-  for (var i = 0; i < 6; i++)
+  for (let i = 0; i < 6; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
   return text;
 }
 
-console.log(makeid());
+export default makeid;
